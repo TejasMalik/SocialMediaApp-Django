0c48ff16e7fdb38df05fda94a0d7883e685fe73c
Malik <tmalik8@dxc.com>
2020-11-23 23:37:50 +0530

    Initial

---
+++ social/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from social import models
+
+# Register your models here.
+admin.site.register([
+    models.Post,
+    models.Friend,
+    models.Like,
+    models.Comment
+])
